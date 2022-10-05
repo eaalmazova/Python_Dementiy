f669a75fc0b8f2e604019e5408ddee7b24985578
Elizaveta Almazova <103960329+eaalmazova@users.noreply.github.com>
2022-10-05 21:20:31 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,39 @@
-import typing as tp
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+text = ''
+
+
+# In[2]:
+
+
+def textwork():
+    global text
+    print('Please, enter the number of abstracts:')
+    abstract = int(input())
+    for i in range(abstract):
+        print('Please, enter the {} abstract of the text:'.format(i+1))
+        text += input()
+
+
+# In[3]:
+
+
+def filework():
+    global text
+    print('Please, enter the name of the txt file from the program folder:')
+    filename = input()
+    if filename.count('.txt') == 0:
+        filename=filename+'.txt'
+    with open(filename) as f:
+        text = f.read()
+
+
+# In[4]:
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
@@ -14,9 +49,20 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    for char in plaintext:
+        if ord(char)>=65 and ord(char)<=90:
+            over = bool((ord(char)+shift)//91)
+            ciphertext+=chr(ord(char)+shift-(90-65+1)*int(over))
+        elif ord(char)>=97 and ord(char)<=122:
+            over = bool((ord(char)+shift)//123)
+            ciphertext+=chr(ord(char)+shift-(122-97+1)*int(over))
+        else:
+            ciphertext+=char
     return ciphertext
+
+
+# In[5]:
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
@@ -32,15 +78,83 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    plaintext = ''
+    for char in ciphertext:
+        if ord(char)>=65 and ord(char)<=90:
+            over = not bool((ord(char)-shift)//65)
+            plaintext+=chr(ord(char)-shift+(90-65+1)*int(over))
+        elif ord(char)>=97 and ord(char)<=122:
+            over = not bool((ord(char)-shift)//97)
+            plaintext+=chr(ord(char)-shift+(122-97+1)*int(over))
+        else:
+            plaintext+=char
     return plaintext
 
 
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+# In[6]:
+
+
+def output(t):
+    out = ''
+    while out != 'n' or out != 'y':
+        print('Do you want to write the resulting text in a file? y/n')
+        out = input()
+        if out == 'n':
+            outputtext(t)
+            return
+        elif out == 'y':
+            print('Please, input the name of the txt file')
+            filename = input()
+            if filename.count('.txt') == 0:
+                filename=filename+'.txt'
+            with open(filename,'w') as f:
+                f.write(t)
+            return
+        else:
+            print('Please, try again.')
+
+
+# In[7]:
+
+
+def outputtext(t):
+    if ans[0]=='e':
+        print('Encrypted text:\n{}'.format(t))
+    else:
+        print('Decrypted text:\n{}'.format(t))
+
+
+# In[ ]:
+
+
+choice = False
+while not choice:
+    ans = ''
+    print('Do you want to encrypt or decrypt? e/d')
+    ans+=input()
+    print('Do you want to open a file or work with a text? f/t')
+    ans+=input()
+    if ans=='ef':
+        filework()
+        output(encrypt_caesar(text))
+        choice = True
+    elif ans=='et':
+        textwork()
+        output(encrypt_caesar(text))
+        choice = True
+    elif ans=='df':
+        filework()
+        output(decrypt_caesar(text))
+        choice = True
+    elif ans=='dt':
+        textwork()
+        output(decrypt_caesar(text))
+        choice = True
+    else:
+        print('Please, try again.')
+
+
+# In[ ]:
+
+
+
